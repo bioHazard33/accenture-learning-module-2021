c90cd9ccc3fc2d84a39842736cd6ddd397bb4f9c
biohazard33 <pahulkalra33viii@gmail.com>
2021-04-23 19:54:56 +0530

    CumulativeSum

--- Arrays/Count.java
+++ Arrays/Count.java
@@ -1,50 +1,44 @@
 import java.util.*;
+
 public class Count {
-public static void main (String[] args) {
-Scanner sc=new Scanner(System.in);
 
-System.out.println("Enter the no of subjects:");
-int no_sub=sc.nextInt();
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-if(no_sub>0 && no_sub<=20)
-{
-int marks[]=new int[no_sub];
+    System.out.println("Enter the no of subjects:");
+    int no_sub = sc.nextInt();
 
-for(int i=0;i<no_sub;i++)
-{
-marks[i]=sc.nextInt();
-}
+    if (no_sub > 0 && no_sub <= 20) {
+      int marks[] = new int[no_sub];
 
-int pass=0,fail=0;
-for(int i=0;i<no_sub;i++)
-{
-if(marks[i]<50)
-{
-fail++;
-}
-else
-{
-pass++;
-}
-}
+      for (int i = 0; i < no_sub; i++) {
+        marks[i] = sc.nextInt();
+      }
 
-if(fail==0)
-{
-System.out.println("Ram passed in all subjects");
-}
-else if(pass==0)
-{
-System.out.println("Ram failed in all subjects");
-}
-else
-{
-System.out.println("Ram passed in "+pass+" subjects and failed in "+fail+" subjects");
-}
-}
-else
-{
-System.out.println("Invalid input range");
-}
+      int pass = 0, fail = 0;
+      for (int i = 0; i < no_sub; i++) {
+        if (marks[i] < 50) {
+          fail++;
+        } else {
+          pass++;
+        }
+      }
 
-}
+      if (fail == 0) {
+        System.out.println("Ram passed in all subjects");
+      } else if (pass == 0) {
+        System.out.println("Ram failed in all subjects");
+      } else {
+        System.out.println(
+          "Ram passed in " +
+          pass +
+          " subjects and failed in " +
+          fail +
+          " subjects"
+        );
+      }
+    } else {
+      System.out.println("Invalid input range");
+    }
+  }
 }
