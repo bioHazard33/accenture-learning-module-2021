285795a615b798b8106290d57bf3c2eb66b02b99
biohazard33 <pahulkalra33viii@gmail.com>
2021-04-24 20:01:16 +0530

    stringSplit

---
+++ Strings/stringSplitUp.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main{
+public static void main(String args[])
+{
+Scanner scn = new Scanner(System.in);
+String sentence = scn.nextLine();
+
+String c=scn.nextLine();
+
+String arr[]=sentence.split(c);
+
+for(int i=0;i<arr.length;i++) System.out.println(arr[i]);
+}
+}
