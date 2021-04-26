9bc1e5e283613b16738b30d68fd4cccd51c88f40
biohazard33 <pahulkalra33viii@gmail.com>
2021-04-26 18:35:47 +0530

    unique characters

--- Strings/printUniqueCharacters.java
+++ Strings/printUniqueCharacters.java
@@ -2,49 +2,45 @@ import java.util.*;
 import java.util.regex.*;
 
 public class printUniqueCharacters {
-public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the sentence:");
-String s=sc.nextLine();
 
-Pattern pattern=Pattern.compile("^[a-zA-Z ]*$");
-Matcher m=pattern.matcher(s);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the sentence:");
+    String s = sc.nextLine();
 
-if(m.matches()==true){
-int ctr=0;
-char t[]=new char[s.length()];
+    Pattern pattern = Pattern.compile("^[a-zA-Z ]*$");
+    Matcher m = pattern.matcher(s);
 
-Map<Character,Integer> h=new LinkedHashMap<Character,Integer>();
+    if (m.matches() == true) {
+      int ctr = 0;
+      char t[] = new char[s.length()];
 
-for(int i=0;i<s.length();i++){
-if(h.containsKey(s.charAt(i))){
-h.put(s.charAt(i),h.get(s.charAt(i))+1);
-}
-else{
-h.put(s.charAt(i),1);
-}
-}
+      Map<Character, Integer> h = new LinkedHashMap<Character, Integer>();
 
-for(Map.Entry me:h.entrySet()){
-if((int)me.getValue()==1){
-t[ctr]=(char)me.getKey();
-ctr++;
-}
-}
+      for (int i = 0; i < s.length(); i++) {
+        if (h.containsKey(s.charAt(i))) {
+          h.put(s.charAt(i), h.get(s.charAt(i)) + 1);
+        } else {
+          h.put(s.charAt(i), 1);
+        }
+      }
 
+      for (Map.Entry me : h.entrySet()) {
+        if ((int) me.getValue() == 1) {
+          t[ctr] = (char) me.getKey();
+          ctr++;
+        }
+      }
 
-if(ctr==0){
-System.out.println("No unique characters");
-}
-else{
-System.out.println("Unique characters:");
+      if (ctr == 0) {
+        System.out.println("No unique characters");
+      } else {
+        System.out.println("Unique characters:");
 
-for(int i=0;i<ctr;i++)System.out.println(t[i]);
-}
-}
-else{
-System.out.println("Invalid Sentence");
-}
-
-}
+        for (int i = 0; i < ctr; i++) System.out.println(t[i]);
+      }
+    } else {
+      System.out.println("Invalid Sentence");
+    }
+  }
 }
