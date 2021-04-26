b5c723c2fe2d708d620270ea8fb9215a7aab871b
biohazard33 <pahulkalra33viii@gmail.com>
2021-04-26 19:07:56 +0530

    employeeIDValidation

---
+++ Strings/employeeIDValidation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class UserMain {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your ID");
+String s=sc.nextLine();
+
+Pattern pattern=Pattern.compile("^GBL/[0-9]{3}/[0-9]{4}$");
+
+Matcher m=pattern.matcher(s);
+
+if(m.matches()==true){
+System.out.println("Login Success");
+}
+else{
+System.out.println("Incorrect ID");
+}
+}
+}
