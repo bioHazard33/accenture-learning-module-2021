a03128f417def443448086d117186563514f9361
biohazard33 <pahulkalra33viii@gmail.com>
2021-04-27 18:02:35 +0530

    Case Stuyd 2

--- Running Case Study 2/UserInterface.java
+++ Running Case Study 2/UserInterface.java
@@ -1,20 +1,20 @@
 Scanner sc = new Scanner(System.in);
 com.sbcc.model.Player obj=new com.sbcc.model.Player();
 while(true){ 
-System.out.println("1.Parse the player details and create player"); 
-System.out.println("2.Exit"); System.out.println("Enter your choice"); 
-int choice=sc.nextInt(); sc.nextLine(); 
-if(choice==1) { 
-System.out.println("Enter the player details"); 
-String s=sc.nextLine(); 
-obj=new com.sbcc.utility.SBCCUtility().parsePlayerDetails(s); 
-System.out.println("Player id:"+obj.getPlayerId()); 
-System.out.println("Player name:"+obj.getPlayerName()); 
-System.out.println("Matches played:"+obj.getMatchesPlayed()); 
-System.out.println("Total runs scored:"+obj.getRunScored()); 
-System.out.println("Playing zone:"+obj.getPlayingZone()); 
-} if(choice==2) { 
-System.out.println("Thank you for using SBCC application"); 
-System.exit(0); 
-} 
+    System.out.println("1.Parse the player details and create player"); 
+    System.out.println("2.Exit"); System.out.println("Enter your choice"); 
+    int choice=sc.nextInt(); sc.nextLine(); 
+    if(choice==1) { 
+        System.out.println("Enter the player details"); 
+        String s=sc.nextLine(); 
+        obj=new com.sbcc.utility.SBCCUtility().parsePlayerDetails(s); 
+        System.out.println("Player id:"+obj.getPlayerId()); 
+        System.out.println("Player name:"+obj.getPlayerName()); 
+        System.out.println("Matches played:"+obj.getMatchesPlayed()); 
+        System.out.println("Total runs scored:"+obj.getRunScored()); 
+        System.out.println("Playing zone:"+obj.getPlayingZone()); 
+    } if(choice==2) { 
+        System.out.println("Thank you for using SBCC application"); 
+        System.exit(0); 
+    } 
 }
