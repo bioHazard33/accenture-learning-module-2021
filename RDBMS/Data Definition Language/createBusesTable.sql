70536b2df6e9f85783e93675d3095c0fbff44ab0
biohazard33 <pahulkalra33viii@gmail.com>
2021-05-24 22:38:18 +0530

    structure

---
+++ RDBMS/Data Definition Language/createBusesTable.sql
@@ -0,0 +1,8 @@
+create table buses(
+    Bus_no number(11),
+    Bus_name varchar2(20),
+    Type varchar2(20),
+    Total_seats number(11),
+    Avail_seats number(11),
+    constraint pk_buses primary key(Bus_no)
+);
