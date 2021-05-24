452ffcf49d8ebbcf99132720594343490c2706ac
biohazard33 <pahulkalra33viii@gmail.com>
2021-05-24 23:01:33 +0530

    alterModifyColumnType

--- RDBMS/Data Definition Language/alterModifyColumnType.sql
+++ RDBMS/Data Definition Language/alterModifyColumnType.sql
@@ -1,10 +1,2 @@
-create table payments(
-    payment_id varchar2(3),
-    constraint pk_payments primary key(payment_id),
-    ticket_id varchar2(3),
-    constraint fk_payments_tickets foreign key(ticket_id) references tickets(ticket_id),
-    bd_id number(11),
-    constraint fk_payments_bookingdetails foreign key(bd_id) references bookingdetails(bd_id),
-    discount_id varchar2(3),
-    constraint fk_payments_discounts foreign key(discount_id) references discounts(discount_id)
-);
+alter table mobile_specification
+modify weight number(10);
