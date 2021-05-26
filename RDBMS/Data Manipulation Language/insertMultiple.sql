3977b530761ccca5f05751e7250d6a2de6a83e27
biohazard33 <pahulkalra33viii@gmail.com>
2021-05-26 23:38:54 +0530

    insertMultiple

---
+++ RDBMS/Data Manipulation Language/insertMultiple.sql
@@ -0,0 +1,16 @@
+insert into customer_info(customer_id, customer_name, address, mobile, email)
+values(
+        'C01',
+        'Amal',
+        'Delhi',
+        1234564566,
+        'amal@gmail.com'
+    );
+insert into customer_info
+values(
+        'C02',
+        'Ajay',
+        'Bangalore',
+        1234565689,
+        'ajay@yahoo.com'
+    );
