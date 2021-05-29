6e6887cef650fedb8006908559b888302aa71e85
biohazard33 <pahulkalra33viii@gmail.com>
2021-05-29 17:29:05 +0530

    selectCustomerBasedOnCondition

--- RDBMS/Select Statements/selectcustomerBasedOnCondition.sql
+++ RDBMS/Select Statements/selectcustomerBasedOnCondition.sql
@@ -1,1 +1,1 @@
-select customer_id,customer_name,email from customer_info where customer_name like 'R%l' order by customer_name;
+select customer_id,customer_name,address,email from customer_info where customer_name like 'R%l' order by customer_name;
